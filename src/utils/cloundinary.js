0a86cae8051ae6ca822153f4ab85104eb7611157
pandoHardik <123441529+pandoHardik@users.noreply.github.com>
2024-01-27 13:44:42 +0530

    add:setup of multer& cloundinary

---
+++ src/utils/cloundinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import fs from "fs"; 
+
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUNDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUNDINARY_API_KEY, 
+  api_secret: process.env.CLOUNDINARY_API_SECRET
+});
+
+
+const uploadCloundinary= async (localFilePath) =>{
+    try {
+        
+        if(!localFilePath ) return null
+      const response=await cloudinary.uploader.upload(localFilePath,{
+            resourse_type:"auto"
+        })
+console.log("upload file sucessfuly on cloundinary",response.url);
+return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove local save temporay file when opreation faild
+        return null
+    }
+}
+
+export {uploadCloundinary}
